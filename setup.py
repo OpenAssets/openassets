5a5f69d2e278569dbed96bf4e8febfb7bd72c907
Flavien Charlon <flavien@charlon.net>
2014-09-02 13:30:31 +0100

    Add the setup file

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import openassets
+import os
+import setuptools
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.rst')) as f:
+    README = f.read()
+
+setuptools.setup(
+    name = "openassets",
+    version = openassets.__version__,
+    packages = [ "openassets" ],
+    description = "Reference implementation of the Open Assets Protocol",
+    author = "Flavien Charlon",
+    url = "https://github.com/OpenAssets/openassets",
+    license = "MIT License",
+    install_requires = [
+        "python-bitcoinlib == 0.1.1"
+    ],
+    test_suite = "tests",
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
